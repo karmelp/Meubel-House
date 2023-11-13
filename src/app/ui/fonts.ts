999bf248ad6c50c8180867a82fe6b8065e832cf7
Karmel Pea <karmelpea@gmail.com>
2023-11-13 17:55:53 +0200

    Contact

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -1,8 +1,8 @@
 import { Poppins } from 'next/font/google';
 
 export const poppins = Poppins({ 
-    weight: ['300', '400', '500', '700'],
-    style: ['normal'],
-    subsets: ['latin'],
-    display: 'swap', 
+  weight: ['300', '400', '500', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap', 
 });
