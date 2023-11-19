dcf00829a399aec2298bd96f33fd54e0e14adf06
Karmel Pea <karmelpea@gmail.com>
2023-11-19 11:18:39 +0200

    About

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -20,6 +20,11 @@ const About = (props: Props) => {
         heroImage={heroImage}
       />
 
+      <div className="about">
+        <p>Meubel House was founded on the simple idea of bringing sustainably sourced, meticulously crafted, impeccably designed, and built-to-last luxury furniture to your home.  At Meubel House, we believe in designing and crafting intuitively iconic pieces of furniture that exude luxury elegance and effortless comfort. We conceptualize and design every piece to be rich in detail, high in comfort, and best in functionality. From visual interest to texture, to shape to the minute tailored details, we meticulously craft our collection of living, dining, and outdoor furniture to become a point of pride at your home.</p>
+        <p>At the heart of Meubel House is our dedicated and skilled team of artisans and craftsmen that we have carefully chosen to work with us and their deep appreciation for the maker’s touch. And we go the extra mile to achieve excellence, thanks to their craftsmanship and several decades of experience behind them. With hand-selection of premium materials considering their texture, uniqueness, and longevity, coupled with a thoughtful selection of tones and designs, we ensure the quality of our pieces is bespoke without forgetting to highlight the inherent beauty of materials. </p>
+      </div>
+
       <InfoSection />
     </div>
   );
