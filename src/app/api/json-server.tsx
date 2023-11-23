d3bb056f0f6021518e7caa5c37dac5ceeb5f8b0b
Karmel Pea <karmelpea@gmail.com>
2023-11-23 13:26:15 +0200

    json

---
+++ src/app/api/json-server.tsx
@@ -0,0 +1,10 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import fetch from 'node-fetch';
+
+const JSON_SERVER_URL = 'https://meubel-house.vercel.app';
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const response = await fetch(`${JSON_SERVER_URL}${req.url}`);
+  const data = await response.json();
+  res.json(data);
+}
