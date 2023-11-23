746296a58902f40ec52af09d6663cfee04652a75
Karmel Pea <karmelpea@gmail.com>
2023-11-23 12:28:58 +0200

    fixes

--- src/app/lib/AuthContext.tsx
+++ src/app/lib/AuthContext.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { ReactNode, createContext, useState } from 'react';
+import { createContext, useState } from 'react';
 
 export const AuthContext = createContext({
   isAuthenticated: false,
