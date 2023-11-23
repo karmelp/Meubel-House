4773152f345fe1348952430cbe6b8567e51b9742
ilkinguluzada <contact@ilkin-guluzada.com>
2023-11-23 13:39:03 +0400

    meta data

--- src/app/(pages)/shop/[id]/page.tsx
+++ src/app/(pages)/shop/[id]/page.tsx
@@ -1,57 +1,44 @@
-'use client';
-import { Metadata, ResolvingMetadata } from 'next';
-import React, { useState, useEffect } from 'react';
+import SingleProductPage from '@/app/ui/components/singleProductPage/SingleProductPage';
 
-import Breadcrumbs from '@/app/ui/components/breadcrumbs/Breadcrumbs';
-import SingleProduct from '@/app/ui/components/singleProduct/SingleProduct';
+const fetchData = async (params: { id: string }) => {
+  try {
+    const productsFetch = await fetch(`http://localhost:3001/products/${params?.id}`, {
+      cache: 'no-store',
+    });
 
-import './singlePage.scss';
+    return await productsFetch.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
 
-const SingleProductPage = ({
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const response: any = await fetch(`${process.env.API_URL}/products/${params?.id}`, {
+    cache: 'no-store',
+  });
+
+  const product = await response.json();
+
+  if (!product) {
+    return {
+      title: 'Page Not Found',
+    };
+  }
+
+  return {
+    title: product.name,
+  };
+}
+
+const SingleProduct = async ({
   params,
 }: {
   params: {
     id: string;
   };
 }) => {
-  const [product, setProduct] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const product = await fetchData(params);
 
-  useEffect(() => {
-    // Fetch data from the API
-    const fetchData = async () => {
-      try {
-        const productsFetch = await fetch(`http://localhost:3001/products/${params?.id}`, {
-          cache: 'no-store',
-        });
-        const Products1 = await productsFetch.json();
-        setProduct(Products1);
-        setLoading(true);
-        console.log('Data for page', Products1);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, [params.id]);
-  console.log('params ', params);
-
-  const breadcrumbs = [
-    { text: 'Home', link: '/' },
-    { text: 'Shop', link: '/shop' },
-    { text: (product as any)?.name, link: `/shop/${encodeURIComponent((product as any)?.name)}` },
-  ];
-
-  return (
-    <>
-      {loading ? (
-        <div className='single-product-page'>
-          <Breadcrumbs items={breadcrumbs} />
-          <SingleProduct product={product as any} />
-        </div>
-      ) : null}
-    </>
-  );
+  return <>{<SingleProductPage product={product} />}</>;
 };
-export default SingleProductPage;
+export default SingleProduct;
